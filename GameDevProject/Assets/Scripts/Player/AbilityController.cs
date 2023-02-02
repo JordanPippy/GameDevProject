49c46c5efb1af005ab159a4774b317b53fb72f92
Jordan <jordanpippy@acadiau.ca>
2023-02-02 09:30:41 -0400

    moved functions into library
    +Also added 2 tilemaps

--- GameDevProject/Assets/Scripts/Player/AbilityController.cs
+++ GameDevProject/Assets/Scripts/Player/AbilityController.cs
@@ -29,18 +29,13 @@ public class AbilityController : MonoBehaviour
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mousePos.z = 0;
 
-        float angle = Mathf.Atan((mousePos.y - transform.position.y) / (mousePos.x - transform.position.x)) * Mathf.Rad2Deg;
+        float angle = Library.MousePlayerAngle(mousePos, transform.position);
         if (mousePos.x > transform.position.x)
             angle += 180;
             
         spell = Instantiate(ability.prefab, transform.position, Quaternion.Euler(0, 0, angle));
         Destroy(spell, 5);
 
-        spell.GetComponent<Rigidbody2D>().AddForce(VectorFromAngle(angle * Mathf.Deg2Rad) * -500.0f);
-    }
-
-    public Vector2 VectorFromAngle (float theta) 
-    {
-        return new Vector2 (Mathf.Cos(theta), Mathf.Sin(theta)); // Trig is fun
+        spell.GetComponent<Rigidbody2D>().AddForce(Library.VectorFromAngle(angle * Mathf.Deg2Rad) * -500.0f);
     }
 }
