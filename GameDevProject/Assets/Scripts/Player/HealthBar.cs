4841a650d67729651e60e6d6ec304f6436942275
Jordan <136734p@acadiau.ca>
2023-03-20 17:45:07 -0300

    HP Text

--- GameDevProject/Assets/Scripts/Player/HealthBar.cs
+++ GameDevProject/Assets/Scripts/Player/HealthBar.cs
@@ -2,19 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class HealthBar : MonoBehaviour
 {
     public Slider slider;
+    public TextMeshProUGUI hpText;
+
+    void Start()
+    {
+        
+    }
 
     public void SetMaxHealth(int health)
     {
         slider.maxValue = health;
         slider.value = health;
+        hpText.text = health + "";
     }
 
     public void SetHealth(int health)
     {
         slider.value = health;
+        hpText.text = health + "";
     }
 }
